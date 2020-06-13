295d6b1544806f5608103a856d424289d5dfbdc6
BrandonKutivan <bskutivan@gmail.com>
2020-06-13 16:27:04 -0700

    found minor spelling error preventing signup view from being delivered. Added delete post and edit post js files

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -98,7 +98,7 @@ router.put('/:id', (req, res) => {
     Post.update(
         {
             title: req.body.title,
-            content: req.body.content
+            contents: req.body.contents
         },
         {
             where: {
